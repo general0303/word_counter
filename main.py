74e48830b557bd78a0ae921aa1cacff0e933ecfe
DELL VORSTO <psh030302@yandex.ru>
2024-02-18 15:13:30 +0300

    построчное чтение файла

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ morph = MorphAnalyzer()
 counter = {"ADJF": 0, "ADJS": 0, "VERB": 0, "INFN": 0, "ADVB": 0}
 
 with codecs.open(input("Введите имя файла\n"), "r", "utf_8_sig") as f:
-    text = f.read()
-word_tokens = nltk.word_tokenize(text.translate(remove_punct_dict))
-for word in word_tokens:
-    part_of_speech = morph.parse(word)[0].tag.POS
-    if part_of_speech in counter:
-        counter[part_of_speech] += 1
+    for text in f:
+        word_tokens = nltk.word_tokenize(text.translate(remove_punct_dict))
+        for word in word_tokens:
+            part_of_speech = morph.parse(word)[0].tag.POS
+            if part_of_speech in counter:
+                counter[part_of_speech] += 1
 print(f"В тексте представлено:\n"
       f"{counter['ADVB']} наречий,\n"
       f"{counter['VERB'] + counter['INFN']} глаголов,\n"
