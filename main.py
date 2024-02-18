ec6da8c58625662242247f4d87ed1685656401fa
DELL VORSTO <psh030302@yandex.ru>
2024-02-18 13:17:58 +0300

    внедрение подсчета слов в разрезе частей речи

--- main.py
+++ main.py
@@ -6,11 +6,13 @@ from pymorphy2 import MorphAnalyzer
 
 remove_punct_dict = dict((ord(p), None) for p in string.punctuation)
 morph = MorphAnalyzer()
+counter = {"ADJF": 0, "ADJS": 0, "VERB": 0, "INFN": 0, "ADVB": 0}
 
 with codecs.open("text.txt", "r", "utf_8_sig") as f:
     text = f.read()
 word_tokens = nltk.word_tokenize(text.translate(remove_punct_dict))
 for word in word_tokens:
-    print(word, morph.parse(word)[0].tag.POS)
-
-print(word_tokens)
+    part_of_speech = morph.parse(word)[0].tag.POS
+    if part_of_speech in counter:
+        counter[part_of_speech] += 1
+print(counter)
