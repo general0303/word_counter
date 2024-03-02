701797ef477592f4f77475a9a3780c96bddc3b37
DELL VORSTO <psh030302@yandex.ru>
2024-03-02 16:22:21 +0300

    тесты

--- main.py
+++ main.py
@@ -4,18 +4,27 @@ import string
 from pymorphy2 import MorphAnalyzer
 
 
-remove_punct_dict = dict((ord(p), None) for p in string.punctuation)
-morph = MorphAnalyzer()
-counter = {"ADJF": 0, "ADJS": 0, "VERB": 0, "INFN": 0, "ADVB": 0}
+def count(filename):
+    remove_punct_dict = dict((ord(p), None) for p in string.punctuation)
+    morph = MorphAnalyzer()
+    counter = {"ADJF": 0, "ADJS": 0, "VERB": 0, "INFN": 0, "ADVB": 0}
+    try:
+        with codecs.open(filename, "r", "utf_8_sig") as f:
+            for text in f:
+                word_tokens = nltk.word_tokenize(text.translate(remove_punct_dict))
+                for word in word_tokens:
+                    part_of_speech = morph.parse(word)[0].tag.POS
+                    if part_of_speech in counter:
+                        counter[part_of_speech] += 1
+    except FileNotFoundError:
+        pass
+    return {'ADVB': counter['ADVB'], 'VERB': counter['VERB'] + counter['INFN'],
+            'ADJ': counter['ADJF'] + counter['ADJS']}
 
-with codecs.open(input("Введите имя файла\n"), "r", "utf_8_sig") as f:
-    for text in f:
-        word_tokens = nltk.word_tokenize(text.translate(remove_punct_dict))
-        for word in word_tokens:
-            part_of_speech = morph.parse(word)[0].tag.POS
-            if part_of_speech in counter:
-                counter[part_of_speech] += 1
-print(f"В тексте представлено:\n"
-      f"{counter['ADVB']} наречий,\n"
-      f"{counter['VERB'] + counter['INFN']} глаголов,\n"
-      f"{counter['ADJF'] + counter['ADJS']} прилагательных")
+
+if __name__ == "__main__":
+    counter = count(input("Введите имя файла\n"))
+    print(f"В тексте представлено:\n"
+          f"{counter['ADVB']} наречий,\n"
+          f"{counter['VERB']} глаголов,\n"
+          f"{counter['ADJ']} прилагательных")
