7b9cca66c027fee1ca06e4ed35d1bc320c97e4a7
DELL VORSTO <psh030302@yandex.ru>
2024-03-23 09:52:06 +0300

    проверка интеграции

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from pymorphy2 import MorphAnalyzer
 def count(filename):
     nltk.download('punkt')
     remove_punct_dict = dict((ord(p), None) for p in string.punctuation)
+
     morph = MorphAnalyzer()
     counter = {"ADJF": 0, "ADJS": 0, "VERB": 0, "INFN": 0, "ADVB": 0}
     try:
